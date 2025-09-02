62ec818dc605121f4d506da7f08e5906d04b9903
Shafiqsarwar76 <shafiq.sarwar@entterprice.com>
2025-09-02 16:50:10 +0500

    changes

--- src/components/ImporterExporter.tsx
+++ src/components/ImporterExporter.tsx
@@ -95,7 +95,7 @@ export default function ImporterExporter() {
                     <li
                       key={i}
                       style={{ color: "#6D6D6C", fontSize: "19px" }}
-                      className="text-start px-1 font-medium"
+                      className="text-start px-1 pe-xl-5 my-2 font-medium"
                     >
                       {item}
                     </li>
