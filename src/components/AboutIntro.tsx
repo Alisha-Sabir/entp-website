628d6b5cd28999740fa737c819daf7438330cd43
Shafiqsarwar76 <shafiq.sarwar@entterprice.com>
2025-09-05 16:46:31 +0500

    text change

--- src/components/AboutIntro.tsx
+++ src/components/AboutIntro.tsx
@@ -4,7 +4,7 @@ export default function AboutIntro() {
   return (
     <div className="my-3">
       <IntroComponent
-        heading="Who we are"
+        heading="Who We Are"
         subHeading="Entterprice is a B2B crossborder payments Fintech using innovative technology for international trade."
         description="We are registered as a Money Services Business (“MSB”) in Canada and have applied for the category 1 Retail Payment Services and Card Schemes (“RPSCS”) license in United Arab Emirates (“UAE”) and the Money Services Operator (“MSO”) license in Hong Kong.
  "
